1be4fc272c2eb66699fa309f26df1ba1279d6bf2
Edin Herić <84852928+hera-beast@users.noreply.github.com>
2022-04-20 01:41:54 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+  //carousel options
+  $('#quote-carousel').carousel({
+    pause: true, interval: 10000,
+  });
+});
+
+
+
+// where, when you need it section
+
+let scrollContainer = document.getElementById('images');
+
+scrollContainer.addEventListener("wheel", (evt) => {
+    evt.preventDefault();
+    scrollContainer.scrollLeft += evt.deltaY * 0.1;
+	
+});
+
+// instagram section
+
+let scrollContainerInstagram = document.getElementById('clean-with-biom'); 
+
+scrollContainerInstagram.addEventListener("wheel", (evt) => {
+    evt.preventDefault();
+    scrollContainerInstagram.scrollLeft += evt.deltaY * 0.1;
+	
+});
+
+// mobile menu show/hide
+
+$(function() {
+'use strict'; $('.hamburger-icon').on('click', function() {
+$('.mobile-menu').toggle();
+});
+});
+
+$(window).resize(function() {
+   'use strict'; if ($(window).width() >= 682) {
+      $(".mobile-menu").hide();
+    }
+  });
